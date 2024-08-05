8865c100fab174fe676449f3567f62a1fca43d7e
TheVoidwalkerrr <121371396+TheVoidwalkerrr@users.noreply.github.com>
2024-08-05 19:40:00 -0300

    Midimix.control.js

--- Midimix.control.js
+++ Midimix.control.js
@@ -1,22 +1,17 @@
 loadAPI(1);
 
-host.defineController("Akai", "Midimix", "1.0", "127e1d40-d43c-11e6-9598-0800200c9a66");
+host.defineController("Custom Akai", "MIDI Mix", "1.0", "127e1d40-d43c-11e6-9598-0800200c9a66");
 host.defineMidiPorts(1, 1);
-
 host.addDeviceNameBasedDiscoveryPair(["MIDI Mix"], ["MIDI Mix"]);
 
-var CC_RANGE_LO = 0;
-var CC_RANGE_HI = 100;
-
-var CC_TRACK_VOLUME_LO = 2;
-var CC_TRACK_VOLUME_HI = 9;
-
-var CC_MASTER_TRACK_VOLUME = 10;
-
-var CC_RANGE_MAPPING = {
+var CC = {
   trackVolume: {
     lo: 2,
     hi: 9
+  },
+  masterVolume: {
+    lo: 61,
+    hi: 61
   },
   trackRecord: {
     lo: 10,
@@ -36,104 +31,85 @@ var CC_RANGE_MAPPING = {
   },
 };
 
-/**
-** @param string mapName one of the keys of the CC_RANGE_MAPPING object
-** @param int the cc number
-**
-*/
 function isCCRangeMapped(mapName, cc)
 {
-  var map = CC_RANGE_MAPPING[mapName];
+  var map = CC[mapName];
   return (cc >= map.lo && cc <= map.hi);
 }
 
 function init()
 {
-  println("- Akai Midimix Controller Script by Arakash.com -");
-
-  host.getMidiInPort(0).setMidiCallback(onMidiPort1);
-  noteIn = host.getMidiInPort(0).createNoteInput("Notes");
-
-  //setup 24 rotary knobs
+  host.getMidiInPort(0).setMidiCallback(onMidi);
   rotaryKnobs = host.createUserControlsSection(24);
-  for(var i = CC_RANGE_MAPPING.knobs.lo; i <= CC_RANGE_MAPPING.knobs.hi; i++)
+  for(var i = CC.knobs.lo; i <= CC.knobs.hi; i++)
   {
-    rotaryKnobs.getControl(i - CC_RANGE_MAPPING.knobs.lo).setLabel("Knob " + i);
-  }
-  /*
-  var trackNum = 1;
-  for(var cc = CC_RANGE_MAPPING.knobs.lo; cc <= CC_RANGE_MAPPING.knobs.hi; cc++)
-  {
-    var knob = cc - CC_RANGE_MAPPING.knobs.lo;
-    if (cc % 3 == 0) trackNum++;
-    rotaryKnobs.getControl(knob).setLabel("Track " + trackNum + " Knob " + knob % 3);
-  }
-  */
+    rotaryKnobs.getControl(i - CC.knobs.lo).setLabel("Knob " + i);
+ }
 
-  //create trackBank for controlling volume, mute and solo of the first 8 tracks.
-  //and master volume too.
-  trackBank = host.createMainTrackBank(8, 0, 0);
+  trackBank = host.createMainTrackBank(16, 0, 0);
+  masterTrack = host.createMasterTrack(0);
 }
 
-
-function onMidiPort1(status, index, value)
+function onMidi(status, index, value)
 {
+  printMidi(status, index, value);
+
   if (isChannelController(status)) {
 
-    // Volume
+// VOLUME
     if (isCCRangeMapped("trackVolume", index)) {
-      println("Changing volume of track " + trackIndex + " to " + value);
-
-      var trackIndex = index - CC_RANGE_MAPPING.trackVolume.lo;
+      var trackIndex = index - CC.trackVolume.lo;
       var channel = trackBank.getChannel(trackIndex);
       channel.getVolume().setRaw(value / 127);
+      return;
+    }
+//MASTER VOLUME
+    if (isCCRangeMapped("masterVolume", index)) {
+      var trackIndex = index - CC.masterVolume.lo;
+      var channel = trackBank.getChannel(trackIndex);
+      masterTrack.getVolume().setRaw(value / 127);
 
       return;
     }
-
-    // Record Arm
-    if (isCCRangeMapped("trackRecord", index))
-    {
-        //Haven't figured out how to do this yet...
-        return;
+//RECARM
+    if (isCCRangeMapped("trackRecord", index)) {
+      var trackIndex = index - CC.trackRecord.lo;
+      var channel = trackBank.getChannel(trackIndex);
+      if (value == 0) {
+        channel.getArm().toggle();
+      }
+      return;
     }
-
-    // mute
+//MUTE
     if (isCCRangeMapped("trackMute", index)) {
-      var trackIndex = index - CC_RANGE_MAPPING.trackMute.lo;
+      var trackIndex = index - CC.trackMute.lo;
       var channel = trackBank.getChannel(trackIndex);
-      //channel.getMute()
-      //doesn't look like there is a getMute similar to getSolo...
+      if (value == 0) {
+        channel.getMute().toggle();
+        sendNoteOn(0, trackIndex, 0);
+      }
       return;
     }
-
-    if (isCCRangeMapped("trackSolo", index))
-    {
-      println("solo index: " + index);
-      var trackIndex = index - CC_RANGE_MAPPING.trackSolo.lo;
+//SOLO
+    if (isCCRangeMapped("trackSolo", index)) {
+      var trackIndex = index - CC.trackSolo.lo;
       var channel = trackBank.getChannel(trackIndex);
-
-      //only toggle solo when we release the button
-      if (value == 0)
-      {
+      if (value == 0) {
         channel.getSolo().toggle();
       }
-
       return;
     }
-
-    if (isCCRangeMapped("knobs", index))
-    {
-      var knob = index - CC_RANGE_MAPPING.knobs.lo;
+//KNOBS
+    if (isCCRangeMapped("knobs", index)) {
+      var knob = index - CC.knobs.lo;
       rotaryKnobs.getControl(knob).set(value, 128);
-
       return;
     }
+
   }
 }
-
-
 function exit()
 {
-  println("Thanks for using Arakash's AKai Midimix Script!");
+  println("Thanks for using Arakash's AKAI Midimix Script!");
+  println("Fixed by TheVoidwalkerrr");
 }
